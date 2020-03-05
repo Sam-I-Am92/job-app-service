cc5956b97ba89f9970e0a93f2682e8d37bae8476
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2020-03-04 20:45:21 -0800

    Setup and check status of jest

---
+++ __test__/jest.test.js
@@ -0,0 +1,7 @@
+// test if jest is working and logging correct information
+
+describe('Set up jest', () => {
+  it('should check status of jest', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // status function sends back time stamp on get request
 var timeLog = (req, res) => {
-  var time = `${Date.now()}`;
+  var time = `Time: ${Date.now()}`;
   res.status(200).send(time);
 };
 
